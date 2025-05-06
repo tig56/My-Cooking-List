2aec9a4837a1178654eca94ef6f6460b9705bc4f
tig56 <taiga06091998@icloud.com>
2025-05-06 11:54:03 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+{
+    const open = document.getElementById('open');
+    const menuBtn = document.querySelector('.menu-btn');const close = document.getElementById('close');
+
+    open.addEventListener('click', () => {
+        menuBtn.classList.add('show');
+        open.classList.add('hide');
+     });
+
+     close.addEventListener('click', () => {
+        menuBtn.classList.remove('show');
+        open.classList.remove('hide');
+     });
+}
